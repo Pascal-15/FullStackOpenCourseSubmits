8a4fdac5eaecb284568cb088bab2a97d90b146a5
Pascal-K <karsten.pas@gmail.com>
2024-04-26 13:28:28 +0200

    Exercises 1.8/1.9 Step 3/4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,40 +1,51 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+  const percentage = (props.good / all * 100)
+
+    if (all <= 0) {
+      return (
+        <div>
+          No feedback given !
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          good: {props.good} <br />
+          neutral: {props.neutral} <br />
+          bad: {props.bad} <br />
+          all: {props.good + props.neutral + props.bad} <br />
+          average: {props.score} <br />
+          positive: {percentage} %
+        </div>
+      )
+    }
+}
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const [all, setAll] = useState(0)
   const [score, setScore] = useState(0)
-  const [percentage, setPercentage] = useState(0)
-
 
   const handleGood = () => {
     setGood(good + 1)
-    setAll(all + 1)
     setScore(score + 1)
-    calculatePercentage()
     console.log("Good: ", good)
   }
   const handleNeutral = () => {
     setNeutral(neutral + 1)
-    setAll(all + 1)
-    calculatePercentage()
     console.log("Neutral: ", neutral)
   }
   const handleBad = () => {
     setBad(bad + 1)
-    setAll(all + 1)
     setScore(score - 1)
-    calculatePercentage()
     console.log("Bad: ", bad)
-  }
-  const calculatePercentage = () => {
-    const calcP = (good / all) * 100
-    setPercentage(calcP)
-    console.log(percentage)
   }
   return (
     <div>
@@ -43,12 +54,7 @@ const App = () => {
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
       <h1>STATISTICS</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>average: {score} </p>
-      <p>positive: {percentage} %</p>
+      <Statistics good={good}neutral={neutral}bad={bad}score={score}/>
     </div>
   )
 }
