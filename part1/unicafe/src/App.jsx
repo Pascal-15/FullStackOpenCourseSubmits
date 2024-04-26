326409d5b951b37855b0038ac59ae348829df471
Pascal-K <karsten.pas@gmail.com>
2024-04-26 12:48:39 +0200

    Exercises 1.7 Step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,16 +6,35 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [all, setAll] = useState(0)
+  const [score, setScore] = useState(0)
+  const [percentage, setPercentage] = useState(0)
+
 
   const handleGood = () => {
     setGood(good + 1)
-
+    setAll(all + 1)
+    setScore(score + 1)
+    calculatePercentage()
+    console.log("Good: ", good)
   }
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
+    calculatePercentage()
+    console.log("Neutral: ", neutral)
   }
   const handleBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    setScore(score - 1)
+    calculatePercentage()
+    console.log("Bad: ", bad)
+  }
+  const calculatePercentage = () => {
+    const calcP = (good / all) * 100
+    setPercentage(calcP)
+    console.log(percentage)
   }
   return (
     <div>
@@ -23,10 +42,13 @@ const App = () => {
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
-      <h2>STATISTICS</h2>
-      <p><label>good: {good} </label></p>
-      <p><label>neutral: {neutral} </label></p>
-      <p><label>bad: {bad} </label></p>
+      <h1>STATISTICS</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {score} </p>
+      <p>positive: {percentage} %</p>
     </div>
   )
 }
